aaf36b48505d1deeaeb0a971d9974649b1949305
JuliusCandra <gagahkrenz23@gmail.com>
2020-07-22 17:04:26 +0700

    Membuat UI design Login form

---
+++ app/src/main/java/com/dev_candra/example_ui_design_login/MainActivity.java
@@ -0,0 +1,14 @@
+package com.dev_candra.example_ui_design_login;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
